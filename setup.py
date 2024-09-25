2e2f44f986ad9f086d3f0c38be85d1d48b8cecec
alexsilva <geniofuturo@gmail.com>
2024-09-25 18:57:27 -0300

    v1.0.0

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from setuptools import setup
 
 setup(
     name='its',
-    version='0.1.0',
+    version='1.0.0',
     url='https://github.com/kennethreitz/its',
     license='BSD',
     author='Kenneth Reitz',
